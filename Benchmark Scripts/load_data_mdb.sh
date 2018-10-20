fe1f1f9477c4121cc4c897a958e87e9663a46018
Santhavathi S <santhavathis@Santhavathis-MacBook-Air.local>
2018-10-20 09:28:51 +0530

    SSB Supporting Files

---
+++ Benchmark Scripts/load_data_mdb.sh
@@ -0,0 +1,7 @@
+#Date values must be specified in the format 'yyyy-mm-dd' for cpimport 
+
+cpimport ssb part part.tbl
+cpimport ssb supplier supplier.tbl
+cpimport ssb customer customer.tbl
+cpimport ssb dimdate dimdate.tbl
+cpimport ssb lineorder lineorder.tbl
